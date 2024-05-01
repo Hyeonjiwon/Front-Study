dad7b2613baa74c83b5db674dcdc5bce91fc896c
Hyeonjiwon <zlone1208@gmail.com>
2024-05-01 17:29:35 +0900

    2024.05.01. SearchForm1

--- React/lecture-react/3-component/src/components/SearchForm.js
+++ React/lecture-react/3-component/src/components/SearchForm.js
@@ -8,18 +8,39 @@ export default class SearchForm extends React.Component {
         };
     }
 
+    handleSubmit(event) {
+        // form이 제출 되면 서버로 다시 페이지 요청을 해서 화면이 갱신되는 것을 막음
+        event.preventDefault();
+
+        // props의 callback 함수 호출
+        this.props.onSubmit(this.state.searchKeyword); 
+    }
+
+    handleReset() {
+        this.props.onReset();
+    }
+
     handleChangeInput(event) {
         const searchKeyword = event.target.value;
+
+        if(searchKeyword.length <= 0 ) {
+            this.handleReset();
+        }
+
         this.setState({ searchKeyword }); // searchKeyword 갱신
     }
 
     render() {
+        // 
         return (
-            <form>
+            <form 
+                onSubmit={(event) => this.handleSubmit(event)}
+                onReset={() => this.handleReset()}
+            >
                 <input
                     type="text"
                     placeholder="검색어를 입력하세요"
-                    autofocus
+                    autoFocus
                     value={this.state.searchKeyword}
                     onChange={(event) => this.handleChangeInput(event)}
                 /> 
