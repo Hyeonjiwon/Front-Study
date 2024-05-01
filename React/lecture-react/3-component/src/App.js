bb895fce4f5c9bbaf7558c9ea51ff0f6b2941ef6
Hyeonjiwon <zlone1208@gmail.com>
2024-05-01 18:09:58 +0900

    2024.05.01. State 끌어올리기

--- React/lecture-react/3-component/src/App.js
+++ React/lecture-react/3-component/src/App.js
@@ -3,8 +3,27 @@ import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { 
+      // SearchForm 컴포넌트 내부에 갖혀 있는 상태
+      // 여러 컴포넌트가 동일한 데이터에 의존하고 있음
+      // 이런 경우 가까운 부모 컴포넌트로 state를 끌어올리는 것이 좋다. => App.js로 이동
+      searchKeyword: "",  
+    };
+  }
+
+  handleChangeInput(searchKeyword) {
+    if(searchKeyword.length <= 0) { // 검색어 지우면 reset 
+      this.handelReset();
+    }
+
+    this.setState({ searchKeyword: searchKeyword });
+  }
+  
   search(searchKeyword) {
-    console.log("TODO: search", searchKeyword);
+    console.log("TODO: search", this.state.searchKeyword);
   }
 
   handelReset() {
@@ -17,6 +36,8 @@ export default class App extends React.Component {
         <Header title="검색" />
         <div className="container">
           <SearchForm 
+            value={this.state.searchKeyword}
+            onChange={(value) => this.handleChangeInput(value)}
             onSubmit={(searchKeyword) => this.search(searchKeyword)}
             onReset={() => this.handelReset()}
           /> 
--- React/lecture-react/3-component/src/components/SearchForm.js
+++ React/lecture-react/3-component/src/components/SearchForm.js
@@ -1,53 +1,41 @@
 import React from  "react";
 
-export default class SearchForm extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            searchKeyword: "",
-        };
-    }
 
-    handleSubmit(event) {
+// 함수 컴포넌트로 변경
+const SearchForm = ({ value, onChange, onSubmit, onReset }) => {
+    const handleSubmit = (event) => {
         // form이 제출 되면 서버로 다시 페이지 요청을 해서 화면이 갱신되는 것을 막음
         event.preventDefault();
 
         // props의 callback 함수 호출
-        this.props.onSubmit(this.state.searchKeyword); 
+        onSubmit(); 
     }
 
-    handleReset() {
-        this.props.onReset();
+    const handleReset = () => {
+        onReset();
     }
 
-    handleChangeInput(event) {
-        const searchKeyword = event.target.value;
-
-        if(searchKeyword.length <= 0 ) {
-            this.handleReset();
-        }
-
-        this.setState({ searchKeyword }); // searchKeyword 갱신
+    const handleChangeInput = (event) => {
+        onChange(event.target.value);
     }
 
-    render() {
-        // 
-        return (
-            <form 
-                onSubmit={(event) => this.handleSubmit(event)}
-                onReset={() => this.handleReset()}
-            >
-                <input
-                    type="text"
-                    placeholder="검색어를 입력하세요"
-                    autoFocus
-                    value={this.state.searchKeyword}
-                    onChange={(event) => this.handleChangeInput(event)}
-                /> 
-                {this.state.searchKeyword.length > 0 && (
-                    <button type="reset" className="btn-reset"></button>
-                )}
-            </form>
-        );
-    }
+    return (
+        <form 
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+        >
+            <input
+                type="text"
+                placeholder="검색어를 입력하세요"
+                autoFocus
+                value={value}
+                onChange={handleChangeInput}
+            /> 
+            {value.length > 0 && (
+                <button type="reset" className="btn-reset"></button>
+            )}
+        </form>
+    )
 }
+
+export default SearchForm; 
