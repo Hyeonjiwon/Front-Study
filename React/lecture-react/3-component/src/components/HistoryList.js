913d1dd683a6f1a06a7eb42a6e8bd2125ba5bbf9
Hyeonjiwon <zlone1208@gmail.com>
2024-05-04 01:19:26 +0900

    2024.05.03. 최근검색어 구현 - 상속

---
+++ React/lecture-react/3-component/src/components/HistoryList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import List from "./List.js"
+import store from "../Store.js"
+import { formatRelativeDate } from "../helpers.js";
+
+// renderItem 구현
+export default class HistoryList extends List {
+    // React.Component componentDidMount 오버라이딩
+    // 컴포넌트가 DOM에 마운트 된 직후에 호출
+    componentDidMount() {
+        const data = store.getHistoryList();
+
+        this.setState({
+            data,  // store에서 키워드 목록을 가져와 state로 업데이트하고 render 함수 호출 -> 이때 자식의 renderItem이 호출
+        })
+    }
+    
+    handleClickRemoveHistory(event, keyword) {
+        // 이벤트 전파 막기
+        // List에 li 엘리먼트도 핸들러가 있기 때문에 차단
+        event.stopPropagation();;
+        store.removeHistory(keyword);
+
+        const data = store.getHistoryList();
+
+        this.setState({
+            data,  // store에서 키워드 목록을 가져와 state로 업데이트하고 render 함수 호출 -> 이때 자식의 renderItem이 호출
+        })
+    }
+
+    renderItem(item, index) {
+        return (
+            <>
+                <span className="date">{formatRelativeDate(item.date)}</span>
+                <span>{item.keyword}</span>
+                <button type="reset" className="btn-reset" onClick={(event) => this.handleClickRemoveHistory(event, item.keyword)}></button>
+            </>
+        )
+    }
+}
