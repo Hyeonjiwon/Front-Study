66795b55100e42002c2677c33b0528ffbcb36234
Hyeonjiwon <zlone1208@gmail.com>
2024-05-04 14:50:11 +0900

    2024.05.04. 최근검색어 구현 - 조합: 컴포넌트 담기 (주석변경)

--- React/lecture-react/3-component/src/components/KeywordList.js
+++ React/lecture-react/3-component/src/components/KeywordList.js
@@ -3,12 +3,13 @@ import List from "./List.js"
 import store from "../Store.js"
 
 // 클래스 컴포넌트로 
-// 리스트 컴포넌트가 조합 형태로 변경되어 state가 없음 
+// List 컴포넌트가 조합 형태로 변경되어 state가 없음 
+// 그래서 KeywordList가 keywordList 관리
 export default class KeywordList extends React.Component {
     constructor() {
         super();
 
-        this.state = {
+        this.state = { 
             keywordList: [],
         }
     }
