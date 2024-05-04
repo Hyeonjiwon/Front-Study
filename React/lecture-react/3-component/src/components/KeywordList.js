d7bf815893f6c5d2795fa8db96abd64947227f78
Hyeonjiwon <zlone1208@gmail.com>
2024-05-04 14:45:31 +0900

    2024.05.04. 최근검색어 구현 - 조합: 컴포넌트 담기

--- React/lecture-react/3-component/src/components/KeywordList.js
+++ React/lecture-react/3-component/src/components/KeywordList.js
@@ -2,27 +2,39 @@ import React from "react";
 import List from "./List.js"
 import store from "../Store.js"
 
-// renderItem 구현
-export default class KeywordList extends List {
-    // 컴포넌트 라이프사이클 사용
-    // React.Component를 상속받은 List를 상속 받았기 때문에 오버라이딩 가능
-    // 컴포넌트가 DOM에 마운트 된 직후에 호출되는 메소드
-    componentDidMount() {
-        const data = store.getKeywordList();
+// 클래스 컴포넌트로 
+// 리스트 컴포넌트가 조합 형태로 변경되어 state가 없음 
+export default class KeywordList extends React.Component {
+    constructor() {
+        super();
 
-        // List를 상속 받았기 때문에 내부에 data가 있음
-        this.setState({
-            data,  // store에서 키워드 목록을 가져와 state로 업데이트하고 render 함수 호출 -> 이때 자식의 renderItem이 호출
-        })
+        this.state = {
+            keywordList: [],
+        }
     }
 
-    renderItem(item, index) {
-        return (
-            <>
-                <span className="number">{index + 1}</span>
-                <span>{item.keyword}</span>
-            </>
-        )
+    componentDidMount() {
+        const keywordList = store.getKeywordList();
+        this.setState({ keywordList });
+    }
+
+    render() {
+      return (
+        <List 
+            data={this.state.keywordList} 
+            onClick={this.props.onClick} 
+            renderItem={(item, index) => { // Render Props = ui를 그리는 용도로 사용하는 props 
+                // 사용하는 측에서 React Element({})를 사용하도록 했기 때문에 
+                // React Element를 반환해야 함
+                return (
+                    <>
+                        <span className="number">{index + 1}</span>
+                        <span>{item.keyword}</span>
+                    </>
+                )
+            }}
+        />
+      )
     }
 }
 
--- React/lecture-react/3-component/src/components/List.js
+++ React/lecture-react/3-component/src/components/List.js
@@ -1,30 +1,20 @@
 import React from "react";
 
-export default class List extends React.Component {
-    constructor() {
-        super(); // 부모 호출
-
-        this.state = {
-            data: [],
-        }
-    }
-
-    // 추천 검색어, 최근 검색어 서로 상세 내용을 그리는 방법이 다름 -> 추상 메소드 생성
-    renderItem(item, index) {
-        throw "TODO: renderItem() 구현"; //  List 클래스를 상속받은 자식 클래스에서 renderItem() 구현 
-    }
-
-    render() {
-      return (
+// 조합: 컴포넌트 담기
+const List = ({ data = [], onClick, renderItem }) => { // renderItem이라는 render props을 받음
+    return (
         <ul className="list">
-            {this.state.data.map((item, index) => { // store에서 데이터를 가져와야 함
+            {data.map((item, index) => { // store에서 데이터를 가져와야 함
                 return (
-                    <li key={item.id} onClick={() => this.props.onClick(item.keyword)}>
-                        {this.renderItem(item, index)}
+                    <li key={item.id} onClick={() => onClick(item.keyword)}>
+                        {/* 추상메소드, props로 주입 */}
+                        {/* renderItem이 반환하는 리액트를 담아서 컴포넌트를 조합하는 방식 */}
+                        {renderItem(item, index)}
                     </li>
                 );
             })}
-        </ul>
-      )
-    }
+        </ul>  
+    )
 }
+
+export default List;
