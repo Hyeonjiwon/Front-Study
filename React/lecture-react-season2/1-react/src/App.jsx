0574d8c90e8ed7f42fdf48aba4584d9f06cb6b9e
hyeonjiwon <zlone1208@gmail.com>
2024-05-16 10:29:31 +0900

    2024.05.16. Button

--- React/lecture-react-season2/1-react/src/App.jsx
+++ React/lecture-react-season2/1-react/src/App.jsx
@@ -1,35 +1,45 @@
 import Button from "./components/Button";
 
 const App = () => (
-    <div className="ProductPage">
-        <div className="Page">
-            <header>
-                <h1>메뉴목록</h1>
-            </header>
-            <main>
-                <ul>
-                    <li>
-                        <div className="ProductItem">
-                            <div className="description">
-                                <h2>고소한 바질 파스타</h2>
-                                <div>6,000원</div> 
-                                <Button>주문하기, 결제하기</Button>
-                            </div>
-                            <div className="thumbnail">
-                                <img src="./images/menu-고소한바질파스타.jpg" alt="고소한바질파스타 6,000원"/> 
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </main>
-            <footer>
-                <nav className="Navbar">
-                    <a className="active" href="#">메뉴목록</a>
-                    <a href="#">주문내역</a>
-                </nav>
-            </footer>
-        </div>
+  <div className="ProductPage">
+    <div className="Page">
+      <header>
+        <h1>메뉴목록</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <div className="ProductItem">
+              <div className="description">
+                <h2>고소한 바질 파스타</h2>
+                <div>6,000원</div>
+                <Button
+                  styleType={"brand"}
+                  onClick={() => console.log("TODO: 주문하기 클릭")}
+                >
+                  주문하기
+                </Button>
+              </div>
+              <div className="thumbnail">
+                <img
+                  src="./images/menu-고소한바질파스타.jpg"
+                  alt="고소한바질파스타 6,000원"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav className="Navbar">
+          <a className="active" href="#">
+            메뉴목록
+          </a>
+          <a href="#">주문내역</a>
+        </nav>
+      </footer>
     </div>
+  </div>
 );
 
 export default App;
