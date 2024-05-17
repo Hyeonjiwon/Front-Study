1be13c4a65f7545d3523573c6bde6597968bff3d
hyeonjiwon <zlone1208@gmail.com>
2024-05-17 11:11:45 +0900

    [2024.05.17] 1.2.5.  ProductItem

--- React/lecture-react-season2/1-react/src/components/Button.jsx
+++ React/lecture-react-season2/1-react/src/components/Button.jsx
@@ -3,12 +3,6 @@ const Button = ({ styleType, block, ...rest }) => {
   if (styleType) className += ` ${styleType}`;
   if (block) className += ` block`;
 
-  const handleClick = () => {
-    // 장바구니로 이동 로직을 넣으면 이 컴포넌트의 역할이 제한된다.
-    // 외부로 열어줘야 한다.
-  };
-
-  // onClick이라는 이름의 콜백함수를 props인자로 전달 받고 onClick 에 전달
   return <button className={className} {...rest} />;
 };
 
