cda5097e6429af230f594aa77dc325405be7dc3f
hyeonjiwon <zlone1208@gmail.com>
2024-06-26 18:14:21 +0900

    [2024.06.26] 1.4.4. Title

--- React/lecture-react-season2/1-react/src/components/Title.jsx
+++ React/lecture-react-season2/1-react/src/components/Title.jsx
@@ -1,7 +1,14 @@
-const Title = ({ children }) => {
+const Title = ({ backUrl, children }) => {
+  if (backUrl) {
     return (
-        <h1>{children}</h1>
+      <>
+        <a href={backUrl} />
+        <h1 style={{ paddingRight: "44px" }}>{children}</h1>
+      </>
     );
-}
+  }
+
+  return <h1>{children}</h1>;
+};
 
 export default Title;
