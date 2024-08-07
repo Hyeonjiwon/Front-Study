9cf8606e66c0e4d6548cd6e44dce854d0afd20c7
Hyeonjiwon <zlone1208@gmail.com>
2024-08-07 22:27:31 +0900

    [fix] .env 수정

--- react-playground/src/index.tsx
+++ react-playground/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const naverMapClientId = process.env.REACT_APP_NAVER_MAP_CLIENT_ID;
+const naverMapClientId = process.env.NAVER_CLIENT_ID;
 
 const renderApp = () => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
