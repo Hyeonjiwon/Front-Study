cf5fc3d182ca2fdf293243d4cf8f52e698adf13c
Hyeonjiwon <zlone1208@gmail.com>
2024-08-16 19:23:15 +0900

    [style] DynamicTable 스타일 수정

--- react-playground/src/components/DynamicTable.tsx
+++ react-playground/src/components/DynamicTable.tsx
@@ -7,12 +7,14 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import styled from "styled-components";
 
 interface Column {
   id: string;
   label: string;
   minWidth?: number;
-  align?: "right" | "left" | "center";
+  colAlign: "inherit" | "left" | "center" | "right" | "justify";
+  rowAlign: "inherit" | "left" | "center" | "right" | "justify";
 }
 
 interface DynamicTableProps {
@@ -22,35 +24,81 @@ interface DynamicTableProps {
 
 const DynamicTable = ({ columns, data }: DynamicTableProps) => {
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
+    <StyledTableContainer component={Paper} elevation={0}>
+      <StyledTable>
+        <StyledTableHead>
+          <StyledTableRow>
             {columns.map((column) => (
-              <TableCell
+              <StyledTableColCell
                 key={column.id}
-                align={column.align}
+                align={column.colAlign || "center"}
                 style={{ minWidth: column.minWidth }}
               >
                 {column.label}
-              </TableCell>
+              </StyledTableColCell>
             ))}
-          </TableRow>
-        </TableHead>
+          </StyledTableRow>
+        </StyledTableHead>
         <TableBody>
           {data.map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
+            <StyledTableRow key={rowIndex}>
               {columns.map((column) => (
-                <TableCell key={column.id} align={column.align}>
+                <StyledTableRowCell
+                  key={column.id}
+                  align={column.rowAlign || "left"}
+                >
                   {row[column.id]}
-                </TableCell>
+                </StyledTableRowCell>
               ))}
-            </TableRow>
+            </StyledTableRow>
           ))}
         </TableBody>
-      </Table>
-    </TableContainer>
+      </StyledTable>
+    </StyledTableContainer>
   );
 };
 
 export default DynamicTable;
+
+const StyledTableContainer = styled(TableContainer)`
+  border-radius: 16px;
+  border: 1px solid #c5c5c5;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  overflow: hidden;
+`;
+
+const StyledTable = styled(Table)`
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledTableHead = styled(TableHead)`
+  align-self: stretch;
+  height: 63px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  border-bottom: 2px solid #c1c1c1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const StyledTableRow = styled(TableRow)`
+  height: 27px;
+`;
+
+const StyledTableColCell = styled(TableCell)`
+  font-size: 18px;
+  color: #1f1f1f;
+`;
+
+const StyledTableRowCell = styled(TableCell)`
+  font-size: 16px;
+  color: #000000;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 0 16px;
+`;
